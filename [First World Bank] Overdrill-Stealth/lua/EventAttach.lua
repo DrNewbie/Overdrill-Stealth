b6f3b71e06134ca707818226aedb43f85b8eec0b
Dr_Newbie <tf2baidonation@gmail.com>
2026-06-27 15:26:38 +0800

    use Hooks

--- [First World Bank] Overdrill-Stealth/lua/EventAttach.lua
+++ [First World Bank] Overdrill-Stealth/lua/EventAttach.lua
@@ -3,27 +3,34 @@ core:import("CoreMissionScriptElement")
 core:import("CoreClass")
 ElementCounter = ElementCounter or class(CoreMissionScriptElement.MissionScriptElement)
 
-if not Global.game_settings or not Global.game_settings.level_id or not Global.game_settings.level_id == "red2" or not Global.game_settings.single_player then
+local ThisModPath = ModPath
+
+local function __Name(__text)
+	return "ODS_"..Idstring(tostring(__text)..ThisModPath):key()
+end
+
+if not Global.game_settings or not Global.game_settings.single_player or not Global.game_settings.level_id or not Global.game_settings.level_id == "red2" then
 	return
 end
 
-local PainfullOverdrill_ElementCounter_on_executed = ElementCounter.on_executed
+if _G[__Name(0)] then return end
+_G[__Name(0)] = true
 
 local PainfullOverdrill = {}
 
-function ElementCounter:on_executed(...)
-	local _id = tostring(self._id)
-	if _id == "132053" or _id == "132056" or _id == "132058" or _id == "132059" or _id == "132061" then
-		if PainfullOverdrill['Ready2CloseVent'] and not PainfullOverdrill[_id] then
-			PainfullOverdrill[_id] = 1
+Hooks:PreHook(ElementCounter, "on_executed", __Name("pre"), function(self, ...)
+	local __id = tostring(self._id)
+	if __id == "132053" or __id == "132056" or __id == "132058" or __id == "132059" or __id == "132061" then
+		if PainfullOverdrill['Ready2CloseVent'] and not PainfullOverdrill[__id] then
+			PainfullOverdrill[__id] = 1
 			PainfullOverdrill['Ready2CloseVent'] = 2
 			self._values.counter_target = 1
 		end
 	end
-	if PainfullOverdrill['Ready2CloseVent'] == 3 and PainfullOverdrill[_id] == 1 then
-		PainfullOverdrill[_id] = 2
+	if PainfullOverdrill['Ready2CloseVent'] == 3 and PainfullOverdrill[__id] == 1 then
+		PainfullOverdrill[__id] = 2
 	end
-	if _id == "106692" or _id == "106946" or _id == "106947" or _id == "101024" then
+	if __id == "106692" or __id == "106946" or __id == "106947" or __id == "101024" then
 		self._values.counter_target = 1
 		PainfullOverdrill['Ready2CloseVent'] = 1
 	end
@@ -31,8 +38,11 @@ function ElementCounter:on_executed(...)
 		PainfullOverdrill['MoreLoot'] = true
 		managers.loot:get_mandatory_bags_data().amount = 84
 	end
-	PainfullOverdrill_ElementCounter_on_executed(self, ...)
-	if PainfullOverdrill['Ready2CloseVent'] == 2 and PainfullOverdrill[_id] and self._values.counter_target <= 0 then
+end)
+
+Hooks:PostHook(ElementCounter, "on_executed", __Name("post"), function(self, ...)
+	local __id = tostring(self._id)
+	if PainfullOverdrill[__id] and PainfullOverdrill['Ready2CloseVent'] == 2 and self._values.counter_target <= 0 then
 		PainfullOverdrill['Ready2CloseVent'] = 3
 		local _door_unit_name = Idstring("units/payday2/equipment/gen_interactable_door_reinforced/gen_interactable_door_reinforced")
 		World:spawn_unit(_door_unit_name, Vector3(2880, 550, 106), Rotation(180, 0, 0))
@@ -63,4 +73,4 @@ function ElementCounter:on_executed(...)
 			element:on_executed()
 		end
 	end
-end
+end)
